d7fe7a2fb50292e49f718ce6526955fa35aac803
Tuan Nguyen <me@tuannguyen.me>
2019-09-19 15:31:23 +0700

    Display temp min & temp max instead of pressure

--- lib/models/weather_forecast_model.dart
+++ lib/models/weather_forecast_model.dart
@@ -1,6 +1,8 @@
 class WeatherForecastModel {
   final DateTime date;
   final int temprature;
+  final int tempratureMax;
+  final int tempratureMin;
   final String icon;
   final double pressure;
   final int humidity;
@@ -9,6 +11,8 @@ class WeatherForecastModel {
   WeatherForecastModel(
     this.date,
     this.temprature,
+    this.tempratureMax,
+    this.tempratureMin,
     this.icon,
     this.pressure,
     this.humidity,
@@ -18,6 +22,8 @@ class WeatherForecastModel {
   WeatherForecastModel.initial()
       : date = null,
         temprature = 0,
+        tempratureMax = 0,
+        tempratureMin = 0,
         icon = '',
         pressure = 0,
         humidity = 0,
@@ -26,6 +32,10 @@ class WeatherForecastModel {
   WeatherForecastModel.fromJson(Map<String, dynamic> json)
       : date = DateTime.parse(json['dt_txt']),
         temprature = double.parse(json['main']['temp'].toString()).toInt(),
+        tempratureMax =
+            double.parse(json['main']['temp_max'].toString()).toInt(),
+        tempratureMin =
+            double.parse(json['main']['temp_min'].toString()).toInt(),
         icon = json['weather'][0]['icon'],
         pressure = double.parse(json['main']['pressure'].toString()),
         humidity = json['main']['humidity'],
--- lib/widgets/weather_forecast.dart
+++ lib/widgets/weather_forecast.dart
@@ -17,8 +17,8 @@ class _WeatherForecastState extends State<WeatherForecast> {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<WeatherForecastService>(
-        builder: (context, weatherForecastService, child) {
+    return Consumer2<WeatherForecastService, SettingsService>(
+        builder: (context, weatherForecastService, settingsService, child) {
       final currentItem = weatherForecastService.items[_currentIndex];
 
       return Container(
@@ -33,14 +33,36 @@ class _WeatherForecastState extends State<WeatherForecast> {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
                 Text(
-                  'PRESSURE',
+                  'TEMPERATURE MAX',
                   style: TextStyle(
                     color: Colors.white,
                     fontSize: 16,
                   ),
                 ),
                 Text(
-                  currentItem.pressure.toString(),
+                  '${currentItem.tempratureMax}°${settingsService.value.unit == 'metric' ? 'C' : 'F'}',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  'TEMPERATURE MIN',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                ),
+                Text(
+                  '${currentItem.tempratureMin}°${settingsService.value.unit == 'metric' ? 'C' : 'F'}',
                   style: TextStyle(
                     color: Colors.white,
                     fontSize: 16,
@@ -98,59 +120,56 @@ class _WeatherForecastState extends State<WeatherForecast> {
             Container(
               width: 366,
               height: 140,
-              child: Consumer<SettingsService>(
-                builder: (context, settingsService, child) =>
-                    ListView.separated(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: weatherForecastService.items.length,
-                  itemBuilder: (context, index) {
-                    final forecastItem = weatherForecastService.items[index];
-                    final backgroundColor = _currentIndex == index
-                        ? Colors.white
-                        : Color.fromARGB(255, 26, 32, 44);
-                    final foregroundColor =
-                        _currentIndex == index ? Colors.black : Colors.white;
+              child: ListView.separated(
+                scrollDirection: Axis.horizontal,
+                itemCount: weatherForecastService.items.length,
+                itemBuilder: (context, index) {
+                  final forecastItem = weatherForecastService.items[index];
+                  final backgroundColor = _currentIndex == index
+                      ? Colors.white
+                      : Color.fromARGB(255, 26, 32, 44);
+                  final foregroundColor =
+                      _currentIndex == index ? Colors.black : Colors.white;
 
-                    return GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          _currentIndex = index;
-                        });
-                      },
-                      child: Container(
-                        width: 86,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(10)),
-                          color: backgroundColor,
-                        ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Icon(
-                              UiHelper.getIconData(forecastItem.icon),
+                  return GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _currentIndex = index;
+                      });
+                    },
+                    child: Container(
+                      width: 86,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        color: backgroundColor,
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Icon(
+                            UiHelper.getIconData(forecastItem.icon),
+                            color: foregroundColor,
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            DateFormat('EEE, hh a').format(forecastItem.date),
+                            style: TextStyle(color: foregroundColor),
+                          ),
+                          SizedBox(height: 6),
+                          Text(
+                            '${forecastItem.temprature}°${settingsService.value.unit == 'metric' ? 'C' : 'F'}',
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
                               color: foregroundColor,
                             ),
-                            SizedBox(height: 10),
-                            Text(
-                              DateFormat('EEE, hh a').format(forecastItem.date),
-                              style: TextStyle(color: foregroundColor),
-                            ),
-                            SizedBox(height: 6),
-                            Text(
-                              '${forecastItem.temprature}°${settingsService.value.unit == 'metric' ? 'C' : 'F'}',
-                              style: TextStyle(
-                                fontSize: 24,
-                                fontWeight: FontWeight.bold,
-                                color: foregroundColor,
-                              ),
-                            ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
-                    );
-                  },
-                  separatorBuilder: (context, index) => SizedBox(width: 2),
-                ),
+                    ),
+                  );
+                },
+                separatorBuilder: (context, index) => SizedBox(width: 2),
               ),
             ),
           ],
